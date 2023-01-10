7908dcfd179e4d73abb94ec99676fbee0b84ab20
info-mic-info <jobsocial94@gmail.com>
2023-01-10 21:29:34 +0100

    Complete

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+        <script>
+        export default {
+            name: "AppTitle",
+            data(){
+                return{
+                    example: 'La mia prima app con Vite!'
+                }
+            }
+        }
+        </script>
+<template lang="">
+    <div>
+        {{example}}
+    </div>
+</template>
+<style>
+    
+</style>
